2ae2ffe90631c86b26b72e649a687c2608a49963
weir0062 <weir0062@algonquinlive.com>
2023-09-25 14:33:08 -0400

    Ю

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
 app.use(express.static('public'));
 
 app.listen(port, () => {
