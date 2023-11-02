611e94c76eb62421b701258f70df5d60f0acae9c
weir0062 <weir0062@algonquinlive.com>
2023-11-02 18:24:55 -0400

    team page modal done

--- public/Assets/JS/team.js
+++ public/Assets/JS/team.js
@@ -1,29 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const openModalButtons = document.querySelectorAll("[data-modal-target]");
-    const closeModalButtons = document.querySelectorAll(".close-button");
-    const overlay = document.getElementById("overlay");
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'block';
+    modal.classList.add('show');
+    document.body.style.overflow = 'hidden'; 
 
-    openModalButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            const modal = document.querySelector(button.dataset.modalTarget);
-            openModal(modal);
-        });
+    modal.querySelector('.modal-overlay').addEventListener('click', function() {
+        closeModal(modalId);
     });
+}
 
-    closeModalButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            const modal = button.closest(".modal");
-            closeModal(modal);
-        });
-    });
-
-    function openModal(modal) {
-        if (modal == null) return;
-        modal.style.display = "block";
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal && modal.style.display === 'block') {
+        modal.classList.remove('show');
+        setTimeout(() => {
+            modal.style.display = 'none';
+        }, 300);
+        document.body.style.overflow = 'auto'; 
     }
-
-    function closeModal(modal) {
-        if (modal == null) return;
-        modal.style.display = "none";
-    }
-});
+}
