c5637993a00d698537c30901b864e5dad2cbdb68
weir0062 <weir0062@algonquinlive.com>
2024-01-19 19:14:21 +0300

    Request Quote made, but functionality is not implemented yet

--- public/Assets/JavaScript/functionality.js
+++ public/Assets/JavaScript/functionality.js
@@ -20,3 +20,31 @@ function topFunction() {
 
 
 
+// Get modal element
+var modal = document.getElementById('requestQuoteModal');
+
+// Get open modal button
+var btn = document.getElementsByClassName('btn-request')[0];
+
+// Get close button
+var span = document.getElementsByClassName('close')[0];
+
+// Listen for open click
+btn.onclick = function() {
+    modal.style.display = 'block';
+}
+
+// Listen for close click
+span.onclick = function() {
+    modal.style.display = 'none';
+}
+
+// Listen for outside click
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
+
+
