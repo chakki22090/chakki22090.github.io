0079181c193c5f1b4bb79e66aadb1e0f65f5198f
weir0062 <weir0062@algonquinlive.com>
2024-03-29 15:13:46 -0400

    Bold text option added v4

--- public/Assets/JavaScript/blog.js
+++ public/Assets/JavaScript/blog.js
@@ -118,7 +118,7 @@ function createPostElement(postData) {
         `; 
     }
 
-    const fullText = FormatText(postData.content); // Полный текст без изменений
+    const fullText = FormatText(postData.content); // Полный отформатированный и с ссылками
     let shortText = fullText.length > 50 ? fullText.substring(0, 50) : fullText; // Обрезаем текст до 50 символов
 
     // Ищем первый перенос строки после 50 символов, чтобы завершить абзац
@@ -142,7 +142,7 @@ function createPostElement(postData) {
     `;
 
     const postTextElement = post.querySelector('.post-text');
-    postTextElement.textContent = shortText; // Добавляем короткий текст с сохранением переносов строк
+    postTextElement.textContent = FormatText(shortText); // Добавляем короткий текст с сохранением переносов строк
 
     post.onclick = function() { openModal(this); };
     return post;
